6275cecc3a03a8bbc1b0542d13acbf578a8ba97d
Phunyakrit <phunyakrit@gmail.com>
2026-01-02 21:34:40 +0700

    lab12_2.cpp complete

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
-#include<ctime>
+
 using namespace std;
 
-long long fibonacci(int);
+int gcd(int ,int); 
+
 int main(){
-     clock_t start = clock();
-    cout << "Result: " << fibonacci(50) << "\n";
-    clock_t end = clock();
-    double elapsed = double(end - start)/CLOCKS_PER_SEC;
-    cout << "Elapsed Time: " << elapsed << " seconds.";
-    return 0;
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x,y);
+    return 0; 
 }
-long long fibonacci(int x){
-    if(x == 0) return 0;
-    if(x == 1) return 1;
-    return fibonacci(x - 1) + fibonacci(x - 2);
+
+int gcd(int x, int y){
+	if(x % y == 0){
+		return y;
+	}else{
+		return gcd(y, x % y);
+	}
 }
